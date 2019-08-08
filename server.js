afb40e87a1d80e68b0e3e91eba8241ffbf3ecdcd
Scott <clearplaid@gmail.com>
2019-08-07 22:08:58 -0400

    organized front end and back end mvc folder structure, set up server and added node package managers needed so far

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(express.bodyParser());
+  
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gamerdb", { useNewUrlParser: true });
+
+// app.listen(PORT, () => {
+//   console.log(`🌎 ==> API server now on port ${PORT}!`);
+// });
+
+io.on('connection', () => { /* … */ });
+server.listen(PORT);
