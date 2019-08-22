800814231ffdabb9871c3db42b25af9e6e3d3a1a
Scott <clearplaid@gmail.com>
2019-08-22 12:49:02 -0400

    build commit

---
+++ client/build/precache-manifest.6a5ce54a3299680bcef78155429bb48e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b087a3aa733eb74681ad05bcc84038e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd975ccd9b8460dcc052",
+    "url": "/static/css/main.017949bb.chunk.css"
+  },
+  {
+    "revision": "e8abdd0d3480ca716ea6",
+    "url": "/static/js/2.92eb5e15.chunk.js"
+  },
+  {
+    "revision": "fd975ccd9b8460dcc052",
+    "url": "/static/js/main.009b052c.chunk.js"
+  },
+  {
+    "revision": "8cace837bfa5ad650089",
+    "url": "/static/js/runtime~main.1f5fde3c.js"
+  },
+  {
+    "revision": "fddaa1339e4e5578cf3e5e96f0ebf88d",
+    "url": "/static/media/37223.fddaa133.jpg"
+  },
+  {
+    "revision": "607d87d98ff7274da2faa9593b19ff10",
+    "url": "/static/media/stone-tile.607d87d9.jpg"
+  }
+]);
